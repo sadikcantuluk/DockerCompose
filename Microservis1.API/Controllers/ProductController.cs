b1346dd0b2df3bfa783ace31831d9956437aa0d5
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2024-08-16 16:03:44 +0300

    Web API projeleri üzerinden Docker Compose yapısı kodlandı.

---
+++ Microservis1.API/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microservis1.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservis1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Save(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
